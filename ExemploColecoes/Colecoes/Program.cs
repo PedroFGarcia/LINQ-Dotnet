1a5dcec7b8e9fd70f25c621b84defca88a546a50
Pedro Garcia <pedro_felipe199713@hotmail.com>
2022-02-26 17:33:14 -0300

    update

--- ExemploColecoes/Colecoes/Program.cs
+++ ExemploColecoes/Colecoes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Colecoes.Helper;
 
 namespace Colecoes
@@ -7,16 +8,83 @@ namespace Colecoes
     {
         static void Main(string[] args)
         {
-            OperacoesArray op = new OperacoesArray();
-            int[] array = new int[5] {6, 3, 8, 1, 9};
+            OperacoesLista opLista = new OperacoesLista();
+            List<string> estados = new List<string>{"SP", "MG","SC"};
+            string[] estadosArray = new string[2] {"RS", "PR"};
 
-            System.Console.WriteLine("Array original");
-            op.ImprimirArray(array);
+            System.Console.WriteLine($"Quanditade de elementos na lista: {estados.Count}");
 
-            op.OrdernarBubbleSort(ref array);
+            opLista.ImprimirListaString(estados);
 
-            System.Console.WriteLine("Array ordenado:");
-            op.ImprimirArray(array);
+            // System.Console.WriteLine("Removendo elemento");
+            // estados.Remove("MG");
+
+            // estados.AddRange(estadosArray);
+            estados.Insert(1, "RJ");
+
+            opLista.ImprimirListaString(estados);
+
+            // OperacoesArray op = new OperacoesArray();
+            // int[] array = new int[5] {6, 3, 8, 1, 9};
+            // int[] arrayCopia = new int[10];
+            // string[] arrayString = op.ConverterParaArrayString(array);
+
+            // int valorProcurado = 8;
+
+            // System.Console.WriteLine($"Capacidade atual do array: {array.Length}");
+
+            // op.RedimensionarArray(ref array, array.Length * 2);
+
+            // System.Console.WriteLine($"Capacidade após redimensionar: {array.Length}");
+
+            // int indice = op.ObterIndice(array, valorProcurado);
+
+            // if(indice > -1)
+            // {
+            //     System.Console.WriteLine("O indice do elemento {0} é: {1}", valorProcurado, indice);
+            // }
+            // else
+            // {
+            //     System.Console.WriteLine("Valor não existe no array");
+            // }
+
+            // int valorAchado = op.ObterValor(array, valorProcurado);
+
+            // if(valorAchado > 0)
+            // {
+            //     System.Console.WriteLine("Encontrei");
+            // }
+            // else
+            // {
+            //     System.Console.WriteLine("Não encontrei");
+            // }
+
+            // bool existe = op.Existe(array, valorProcurado);
+
+            // if(existe)
+            // {
+            //     System.Console.WriteLine("Encontrado {0}", valorProcurado);
+            // }
+            // else
+            // {
+            //     System.Console.WriteLine("Não encontrado {0}", valorProcurado);
+            // }
+
+            // System.Console.WriteLine("Array original");
+            // op.ImprimirArray(array);
+
+            // op.OrdernarBubbleSort(ref array);
+            // op.Ordernar(ref array);
+
+            // System.Console.WriteLine("Array ordenado:");
+            // op.ImprimirArray(array);
+
+            // System.Console.WriteLine("Array antes da cópia:");
+            // op.ImprimirArray(arrayCopia);
+
+            // op.Copiar(ref array, ref arrayCopia);
+            // System.Console.WriteLine("Array pós copia");
+            // op.ImprimirArray(arrayCopia);
 
             // int[,] matriz = new int[4,2]
             // {
